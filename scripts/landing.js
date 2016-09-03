262666ec04225c5af6247a885091db72408d1648
Steve Wichelecki <swichelecki@gmail.com>
2016-09-03 13:50:37 -0500

    add scripts/landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var revealPoint = function() {
-                
-        var points = document.getElementsByClassName('point');
-                
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+                          
         for (i = 0; i < points.length; i++) {
             points[i].style.opacity = 1;
             points[i].style.transform = "scaleX(1) translateY(0)";
@@ -27,3 +27,19 @@ var revealPoint = function() {
             //revealSecondPoint();
             //revealThirdPoint();
 };
+
+window.onload = function() {
+    // automatically animate points on tall screens on page load
+    if (window.innerHeight > 950) { 
+        animatePoints(pointsArray);
+        }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { animatePoints(pointsArray);
+        }
+    });       
+    
+}
