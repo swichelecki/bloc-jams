ccc30de9bd9cbfa8ad045881ef0a014b8a813d23
Steve Wichelecki <swichelecki@gmail.com>
2016-09-03 16:17:42 -0500

    checkpoint 23 assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {                   
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)"; 
+};
+    
+
 var animatePoints = function(points) {
-                          
-        for (i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-        }
+    forEach(points, revealPoint);
                 
      //   var revealSecondPoint = function() {
          // points[1].style.opacity = 1;
