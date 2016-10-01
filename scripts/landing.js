fce12d642e8aaa8081b6f7de45d09d7309d00ccf
Steve Wichelecki <swichelecki@gmail.com>
2016-10-01 16:10:17 -0500

    add checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,15 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    
+    $.each($('.point'), revealPoint);
+};
+
+/* var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
     var revealPoint = function(index) {            
@@ -8,49 +19,32 @@ var animatePoints = function(points) {
             points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
         };
     
-    for (var i = 0; i < points.length; i++) {
+    for (i = 0; i < points.length; i++) {
         revealPoint(i);
     }
-     //   var revealSecondPoint = function() {
-         // points[1].style.opacity = 1;
-         // points[1].style.transform = "scaleX(1) translateY(0)";
-         // points[1].style.msTransform = "scaleX(1) translateY(0)";
-         // points[1].style.WebkitTransform = "scaleX(1) translateY(0)";     
-       // };
-                
-    //    var revealThirdPoint = function() {
-        // points[2].style.opacity = 1;
-        // points[2].style.transform = "scaleX(1) translateY(0)";
-        // points[2].style.msTransform = "scaleX(1) translateY(0)";
-        // points[2].style.WebkitTransform = "scaleX(1) translateY(0)";  
-       // };
-                
-            //revealFirstPoint();
-            //revealSecondPoint();
-            //revealThirdPoint();
-};
+   
+}; */
 
-window.onload = function() {
+//window.onload = function() 
+$(window).load(function(){
     // automatically animate points on tall screens on page load
-    if (window.innerHeight > 950) { 
-        animatePoints(pointsArray);
+   // if (window.innerHeight > 950) { 
+       // animatePoints(pointsArray);
+       if ($(window).height() > 950) {
+           animatePoint();
         }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+   // var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   // var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
-    window.addEventListener('scroll', function(event){
-        // if (something && somethingElse)
-      if ((document.documentElement.scrollTop || document.body.scrollTop) >= scrollDistance) { animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+ //   window.addEventListener('scroll', function(event){
+      $(window).scroll(function(event) {
+     // if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { //animatePoints(pointsArray);
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });       
     
-}
-
-/*
-if (falsey) {
-    // would not run
-}
-
-falsey = null, undefined, false, "", 0
-truthy = anything that isn't falsey
+});
